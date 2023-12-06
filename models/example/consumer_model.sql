ce2c2625cc9e42bd8d4945d5d9e290c9ab1bcbe2
Trang Tran <tran@buildupstream.com>
2023-12-06 08:15:43 +0700

    Update dbt

--- models/example/consumer_model.sql
+++ models/example/consumer_model.sql
@@ -2,7 +2,15 @@
 
 with consumer as (
     select last_name, source_id, deliver_addresses, first_name
-    from `project_b79ec5a18f70480b83dcc0d656b2d3d8`.consumer
+    from project_b79ec5a18f70480b83dcc0d656b2d3d8.consumer
+),
+
+
+final as (
+    select lower(last_name) as last_name, source_id, deliver_addresses, first_name
+    from consumer
 )
 
-select * from consumer
+select * from final
+
+
